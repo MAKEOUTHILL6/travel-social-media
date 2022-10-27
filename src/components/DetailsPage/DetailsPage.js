7d7b0a2e83dbed3646bab28edba48dd75a3742e8
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-10-27 19:59:35 +0300

    Fixed rerender problem in discover section

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -12,7 +12,43 @@ export const DetailsPage = () => {
     const publication = publications.find(x => x._id === postId);
 
 
-    return(
-        <h1>Yo</h1>
+    return (
+        <>
+            <section id="details-page">
+                <article class="details-card">
+
+                    <article class="details-card-image">
+
+                        <div class="details-image-wrapper">
+                            <img src='https://lp-cms-production.imgix.net/features/2018/02/Aleksander-Nevski-Cathedral-Sofia-6303650c1b02.jpg'
+                                alt="art-image2" />
+                        </div>
+
+                    </article>
+
+                    <article class="details-card-text">
+                        <h2>Title:<br /> Sofia</h2>
+                        <h3>Posted By: User</h3>
+                        <h2>Description:<br /><span class="desc-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Dolores beatae
+                            magnam quas fuga, dignissimos, veritatis blanditiis quisquam debitis voluptatibus sapiente nemo
+                            corrupti dolor nisi perferendis eaque perspiciatis placeat, accusantium eum.</span></h2>
+
+
+                        <div class="details-button-wrap">
+                            <button class="details-edit">Edit Post</button>
+                            <button class="details-delete">Delete Post</button>
+                        </div>
+
+                    </article>
+
+                    <div class="close-button">
+                        <button />
+                    </div>
+
+                </article>
+            </section>
+        </>
+
     )
 }
--- src/components/DiscoverHeroSection/DiscoverHeroSection.js
+++ src/components/DiscoverHeroSection/DiscoverHeroSection.js
@@ -1,17 +1,24 @@
 import { Publication } from './Publication';
-import PublicationContext from '../../services/PublicationContext';
-import { useContext } from 'react';
+import { useState, useEffect } from 'react';
+import { getPublications } from '../../api/data';
 
 export const DiscoverHeroSection = () => {
 
-    
-    const {publications} = useContext(PublicationContext);
+
+    const [publications, setPublications] = useState([]);
+
+    useEffect(() => {
+        getPublications()
+            .then(res => {
+                setPublications(res);
+            })
+    }, [])
 
     return (
         <main id="discover-main">
             <div id="publication-container">
 
-                {publications.length > 0 ? publications.map(x => <Publication key={x._id} publication={x} />): <h3 className='no-posts'>No publications yet.</h3>}
+                {publications.length > 0 ? publications.map(x => <Publication key={x._id} publication={x} />) : <h3 className='no-posts'>No publications yet.</h3>}
 
             </div>
         </main>
